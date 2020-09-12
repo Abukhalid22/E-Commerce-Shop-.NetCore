4b1b61a348e9b3b3e4b9d73f0716e181a674dfb9
Abdinasir Hussein <husseinfinland94@hotmail.com>
2020-09-12 03:01:29 +0100

    added basket components and checkout, improved user interface for client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { BasketService } from './basket/basket.service';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'Skinet';
 
-
-  constructor() { }
+  constructor(private basketService: BasketService) { }
 
   ngOnInit(): void {
-   
+    const basketId = localStorage.getItem('basket_id');
+    if (basketId) {
+      this.basketService.getBasket(basketId).subscribe(() => {
+        console.log('initialised basket');
+      }, error => {
+        console.log(error);
+      });
+    }
   }
 }
